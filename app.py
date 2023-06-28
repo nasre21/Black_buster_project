c6df381269f14e47064c3d84a8a02ca73b72faa4
YorlinPacha <yorlin1517@gmail.com>
2023-06-28 22:46:20 +0200

    Busqueda por titulo con templates y cantidd listo!!!

--- app.py
+++ app.py
@@ -128,5 +128,15 @@ def login_route():
 def panel():
     return "hola"
 
+#######################YORLIN #####################
+#Creando mi Decorador para la consulta de la pelicula
+
+app.route("/buscar-titulo", methods=["GET", "POST"])(buscarTitulo)
+
+#Redireccionando cuando la página no existe
+# @app.errorhandler(404)
+# def not_found(error):
+#         return render_template("busqueda.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
